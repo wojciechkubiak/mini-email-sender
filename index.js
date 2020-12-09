7022bb765dff411b70c2713a25c2c5698de20cd1
Wojtek Kubiak <wojciechkubiak@pm.me>
2020-12-09 18:45:58 +0100

    Nodemon dependency

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
     const date = new Date();
+    res.setHeader('Content-Type', 'text/plain');
 
     let sql = `INSERT INTO mails(SENDER, SUBJECT, MAIL, DATE_SENT) VALUES (?)`;
     let values = [
